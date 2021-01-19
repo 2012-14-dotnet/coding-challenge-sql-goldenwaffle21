30291276cc94889f9a49f2134e781cfb293bf0f6
elliot <elliot.reid@revature.net>
2021-01-19 08:20:51 -0800

    final

--- dml.sql
+++ dml.sql
@@ -11,9 +11,28 @@ INSERT INTO Employee VALUES(1,10,'12 Gereaux Lane','',Austin,TX,USA);
 INSERT INTO Employee VALUES(2,5,'13 Gereaux Boulevard','',Tacoma,WA,USA);
 INSERT INTO Employee VALUES(3,15,'11 Gereaux Place','',Houston,TX,USA);
 
+GO;
+
 --Add Marketing department
 INSERT INTO Department VALUES(4,Marketing,Quilcene);
+GO;
 
 --Add employee Tina Smith
 INSERT INTO Employee VALUES(1,Tina,Smith,333333333,3);
-INSERT INTO Employee VALUES(1,10,'12 Gereaux Lane','',Toronto,DC,Canada);
+INSERT INTO Employee VALUES(1,8,'7 Gereaux Ave','Back Unit 2',Toronto,DC,Canada);
+GO;
+
+--List all employees in marketing
+SELECT * FROM Employee WHERE DepID=4;
+GO;
+
+--Report total salary of marketing
+SELECT SUM(Salary) FROM Employee WHERE DepID=4;
+GO;
+
+--Report the total employees for each department (my brain is failing me on this one; all I can think of is a UNION of separate counts for each but I'm out of time)
+
+--Increase Tina's salary to 90,000
+UPDATE Employee
+SET Salary=90000 WHERE ID=4;
+GO;
